18f8aecfee98846fc31f148ab587f87a3f6adc71
walnutscreation <walnutscreation@gmail.com>
2023-03-27 19:11:31 +0530

    add files

---
+++ src/app/myCom/todos/todos.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from "./Todo";
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos: Todo[];
+  constructor() {
+
+    this.todos = [
+
+      {
+        sno: 1,
+        title: "This is title",
+        desc: "Descreption",
+        active: true
+      },
+
+      {
+        sno: 2,
+        title: "This is title2",
+        desc: "Descreption",
+        active: true
+      },
+
+      {
+        sno: 3,
+        title: "This is title3",
+        desc: "Descreption",
+        active: true
+      }
+    ]
+  }
+  ngOnInit(): void {
+    //normal forloop
+    // for(var i=0;i<this.todos.length;i++){
+    //   console.log(this.todos[i])
+    // }
+  }
+
+}
